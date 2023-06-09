df6e41afd60700a94aaa04448ae1a8c51d01bdae
Clay Wintringham <aikenwintringham@outlook.com>
2023-06-10 00:08:06 +0800

    first commit

---
+++ sanity.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'sanity'; 
+import {deskTool} from "sanity/desk";
+
+import schemas from './sanity/schemas';
+
+const config = defineConfig({
+    projectId: "uw562szl",
+    dataset: "production",
+    title: "My Personal Website",
+    apiVersion: "2023-06-09",
+    basePath:"/admin",
+    plugins: [deskTool()],
+    schema: { types: schemas}
+})
+
+export default config;
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+    projectId: "uw562szl",
+    dataset: "production",
+    apiVersion: "2023-06-09",
+}
+
+export default config;
